2ea3781d23b18162c5dbe3342260bc52aa6669ea
Avish Tripathi <54525715+Avish-Tripathi@users.noreply.github.com>
2020-06-12 11:33:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def predict():
     return render_template('Home.html', prediction_text='Predicted Percentile is {}'.format(output))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
