6212598da40abe471f2f252c49b1de46dbbea41f
Avish Tripathi <54525715+Avish-Tripathi@users.noreply.github.com>
2020-06-12 17:16:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,17 @@ model = pickle.load(open('model.pkl','rb'))
 def home():
     return render_template('Home.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST','GET'])
 def predict():
     features = [float(x) for x in request.form.values()]
     final = [np.array(features)]
     predict = model.predict(final)
 
-    output = round(predict[0], 4)
-
-    return render_template('Home.html', prediction_text='Predicted Percentile is {}'.format(output))
+    output = float(round(Predict[0], 4))
+    if(output > 100):
+        return render_template('Home.html', prediction_text='Predicted Percentile is {}'.format('100'))
+    else:
+        return render_template('Home.html', prediction_text='Predicted Percentile is {}'.format(output))
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
